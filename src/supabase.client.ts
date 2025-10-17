992f7315e9c95c59dfc5eb4deb23b41d8b9d3fd0
salman <salmancrc9@gmail.com>
2025-10-17 17:37:21 +0500

    feat: add environment variable support and integrate mkcert plugin

--- src/supabase.client.ts
+++ src/supabase.client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://gqjyzanguzequcbrsscr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxanl6YW5ndXplcXVjYnJzc2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4MDc4ODMsImV4cCI6MjA3NTM4Mzg4M30.N20VMweSJLkBrJMuU05K_DHv-smx6v2e8PsGHPxM9z8"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
